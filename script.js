b2b53e831f3cce2874c3a85d9c2239420d0d184f
Shariful Islam <90764070+Shajjad002@users.noreply.github.com>
2024-10-26 18:34:56 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const apiKey="kr3sSHjr0oNEdmdYJYj0tzTiDO_nboGwv-3ki81j4WU";
+//const apiUrl="https://api.unsplash.com/search/photos?page=1&query=office&client_id=kr3sSHjr0oNEdmdYJYj0tzTiDO_nboGwv-3ki81j4WU"
+
+const searchFrom = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult= document.getElementById("search-result");
+const showMore = document.getElementById("show-more");
+
+
+
+let keyword="";
+let page =1;
+
+async function searchImages() 
+{
+    keyword=searchBox.value;
+    const url =`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${apiKey}&per_page=12`;
+
+    const response= await fetch(url);
+    const data = await response.json();
+
+    if(page===1)
+    {
+        searchResult.innerHTML = "";
+    }
+
+    //console.log(data);
+
+    const results= data.results;
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src=result.urls.small;
+
+        const imageLink=document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target="_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+
+    })
+    showMore.style.display="block";
+
+}
+
+searchFrom.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    page=1;
+    searchImages();
+})
+
+showMore.addEventListener("click",()=>{
+    page++;
+    searchImages();
+})
